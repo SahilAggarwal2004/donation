4c0c9a3ff4626837160fd2c4a8fea667d46b0783
vipulgoel30 <96120213+vipulgoel30@users.noreply.github.com>
2022-08-22 18:45:52 +0530

    some changes

--- src/components/Ngo/Allngo.js
+++ src/components/Ngo/Allngo.js
@@ -7,19 +7,19 @@ import { getNgos } from "../../firebase";
 
 const Allngo = () => {
   const [dashboardAnimation, setDashboardAnimation] = useState("");
-  const [ngos, setNgos] = useState()
+  const [ngos, setNgos] = useState();
 
   useEffect(() => {
     setTimeout(() => {
       setDashboardAnimation("hidden");
     }, 1000);
-    getNgos().then(({ success, ngos }) => { if (success) setNgos(ngos) })
-  }, [])
+    getNgos().then(({ success, ngos }) => {
+      if (success) setNgos(ngos);
+    });
+  }, []);
 
   return (
     <>
-      <Navbar />
-
       {/* Animation of dahboard for showing palete */}
       <div className={`my-24 sm:mt-8 px-16 ${dashboardAnimation}`}>
         <div class=" shadow-2xl rounded-md p-12 max-w-lg w-full ">
@@ -41,45 +41,49 @@ const Allngo = () => {
 
       {/* for the showcase of exact data */}
       <div
-        className={`${dashboardAnimation === "" ? "hidden" : ""
-          }  mt-24 sm:mt-8`}
+        className={`${
+          dashboardAnimation === "" ? "hidden" : ""
+        }  mt-24 sm:mt-8`}
       >
         <div className=" px-2 xsm:px-12 grid xlg:grid-cols-3 lg:grid-cols-2 sm:grid-cols-1 gap-12 ">
-          {ngos?.map(({ uid, name, image, mobile, description }) => <div key={uid} className="shadow-[0_1px_16px_2px_rgba(0,0,0,.2)] rounded-xl  px-2 sm:px-8 py-6 flex  flex-col gap-6 bg-slate-50  hover:text-colorPrimary hover:border-colorPrimary hover:border-2 transition-all duration-200 ease-in-out ">
-            <div className="flex gap-10">
-              <img
-                src={image || dummyImage}
-                className="w-24 h-24  rounded-full shadow-2xl"
-              />
-              <div className="flex flex-col gap-1">
-                <div className="text-2xl tracking-widest text-neutral-500 font-light">
-                  {name}
-                </div>
-                <div className="text-base tracking-wide">
-                  <span className="text-lg font-semibold tracking-wider pr-1">
-                    Phone No :
-                  </span>
-                  {mobile}
+          {ngos?.map(({ uid, name, image, mobile, description }) => (
+            <div
+              key={uid}
+              className="shadow-[0_1px_16px_2px_rgba(0,0,0,.2)] rounded-xl  px-2 sm:px-8 py-6 flex  flex-col gap-6 bg-slate-50  hover:text-colorPrimary hover:border-colorPrimary hover:border-2 transition-all duration-200 ease-in-out "
+            >
+              <div className="flex gap-10">
+                <img
+                  src={image || dummyImage}
+                  className="w-24 h-24  rounded-full shadow-2xl"
+                />
+                <div className="flex flex-col gap-1">
+                  <div className="text-2xl tracking-widest text-neutral-500 font-light">
+                    {name}
+                  </div>
+                  <div className="text-base tracking-wide">
+                    <span className="text-lg font-semibold tracking-wider pr-1">
+                      Phone No :
+                    </span>
+                    {mobile}
+                  </div>
                 </div>
               </div>
-            </div>
-            <div>
-              <span className="text-xl font-medium ">Description:</span>
-              <div className="text-neutral-700 pt-2">
-                {description}
-              </div>
-            </div>
-            <div className="flex flex-col gap-2 sm:flex-row sm:gap-0  justify-between items-center ">
               <div>
-                <Link
-                  to={`/ngo/id/${uid}`}
-                  className="bg-gradient-to-r from-colorPrimary  to-colorSecondary text-white rounded-2xl px-6 py-2 text-xl font-medium  hover:border-2 transition-all duration-200 ease-in-out "
-                >
-                  More info
-                </Link>
+                <span className="text-xl font-medium ">Description:</span>
+                <div className="text-neutral-700 pt-2">{description}</div>
+              </div>
+              <div className="flex flex-col gap-2 sm:flex-row sm:gap-0  justify-between items-center ">
+                <div>
+                  <Link
+                    to={`/ngo/id/${uid}`}
+                    className="bg-gradient-to-r from-colorPrimary  to-colorSecondary text-white rounded-2xl px-6 py-2 text-xl font-medium  hover:border-2 transition-all duration-200 ease-in-out "
+                  >
+                    More info
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>)}
+          ))}
         </div>
       </div>
     </>
