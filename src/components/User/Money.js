348905d5b744a4931bd532f0654af33799288bd8
Sahil <aggarwalsahil2004@gmail.com>
2022-08-22 17:02:02 +0530

    commit

--- src/components/User/Money.js
+++ src/components/User/Money.js
@@ -1,23 +1,13 @@
-import React,{useRef} from 'react'
+import React from 'react'
 import about1 from '../../images/about1.png'
 import { toast } from 'react-toastify'
-import { useNavigate } from 'react-router-dom'
 
-const Money=() => {
-    // const navigate = useNavigate()
-    // const amount = useRef()
-    // const ngo = useRef()
-  
+const Money = () => {
 
-    // async function submit(event) {
-    //     event.preventDefault()
-    //     if (type === 'User') {
-    //       const { success, error } = await signupUser(amount.current.value, ngo.current.value)
-    //       if (!success) return toast.error(error)
-    //       toast.success('Amount Deposited')
-    //       navigate('/user')
-    //     }
-    //   }
+    async function submit(event) {
+        event.preventDefault()
+        toast.success('Thanks for donating')
+    }
 
     return (
         <>
@@ -30,15 +20,15 @@ const Money=() => {
                         <div className='p-8 items-center w-1/2'>
                             <h1 className='font-bold text-xl m-1'>Please provide the following information for donations:</h1>
                             <div className='flex flex-col'>
-                                <form >
+                                <form onSubmit={submit}>
                                     <input className='rounded-md w-4.8 px-2 py-1 mt-2 m-1 bg-gray-100 hover:bg-white' type="text" placeholder='Name' />
                                     <input className='rounded-md w-4.8 px-2 py-1 m-1 bg-gray-100 hover:bg-white' type="email" placeholder='Email' />
                                     <input className='rounded-md w-4.8 px-2 py-1 m-1 bg-gray-100 hover:bg-white' type="phone" placeholder='Mobile no.' />
                                     <input className='rounded-md w-4.8 px-2 py-1 m-1 bg-gray-100 hover:bg-white' type="money" placeholder='Amount to be donated' />
                                     <input className='rounded-md w-9.8 px-2 py-1 m-1 bg-gray-100 hover:bg-white' type="text" placeholder='NGO name' />
+                                    <button className="font-semibold my-2 mx-auto btn bg-gray-200 rounded-lg px-4 py-2 m-2">Submit</button>
                                 </form>
-                                <button className="font-semibold my-2 mx-auto btn bg-gray-200 rounded-lg px-4 py-2 m-2">Submit</button>
-                                
+
                             </div>
                         </div>
                     </div>
