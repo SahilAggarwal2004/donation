7c4a4fc3954137443e4618b6544320f93325a89b
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2022-08-22 15:29:47 +0530

    SA 14

--- src/components/User/Money.js
+++ src/components/User/Money.js
@@ -3,7 +3,7 @@ import about1 from '../../images/about1.png'
 import { toast } from 'react-toastify'
 import { useNavigate } from 'react-router-dom'
 
-const Register = ({ type }) => {
+const Money=() => {
     // const navigate = useNavigate()
     // const amount = useRef()
     // const ngo = useRef()
@@ -30,12 +30,12 @@ const Register = ({ type }) => {
                         <div className='p-8 items-center w-1/2'>
                             <h1 className='font-bold text-xl m-1'>Please provide the following information for donations:</h1>
                             <div className='flex flex-col'>
-                                <form  onSubmit={submit}>
+                                <form >
                                     <input className='rounded-md w-4.8 px-2 py-1 mt-2 m-1 bg-gray-100 hover:bg-white' type="text" placeholder='Name' />
                                     <input className='rounded-md w-4.8 px-2 py-1 m-1 bg-gray-100 hover:bg-white' type="email" placeholder='Email' />
                                     <input className='rounded-md w-4.8 px-2 py-1 m-1 bg-gray-100 hover:bg-white' type="phone" placeholder='Mobile no.' />
-                                    <input className='rounded-md w-4.8 px-2 py-1 m-1 bg-gray-100 hover:bg-white' type="money" ref={amount} placeholder='Amount to be donated' />
-                                    <input className='rounded-md w-9.8 px-2 py-1 m-1 bg-gray-100 hover:bg-white' type="text" ref={ngo} placeholder='NGO name' />
+                                    <input className='rounded-md w-4.8 px-2 py-1 m-1 bg-gray-100 hover:bg-white' type="money" placeholder='Amount to be donated' />
+                                    <input className='rounded-md w-9.8 px-2 py-1 m-1 bg-gray-100 hover:bg-white' type="text" placeholder='NGO name' />
                                 </form>
                                 <button className="font-semibold my-2 mx-auto btn bg-gray-200 rounded-lg px-4 py-2 m-2">Submit</button>
                                 
